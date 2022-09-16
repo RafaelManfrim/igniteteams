12e9567dfadcd745cc6a416aca815822adc63800
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-09-15 22:46:07 -0300

    Carregando fonte Roboto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
-import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
+import { ActivityIndicator } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
 
+import theme from './src/theme';
 import { Groups } from './src/screens/Groups'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
