3767a32e1e0c1bd5c6a04d97a4f72a3d15c82475
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-09-16 20:24:00 -0300

    Renderizando lista de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 
 import { GroupsContainer } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Galera do Ignite', 'Galera do barulho'])
+
   return (
     <GroupsContainer>
       <Header />
       <Highlight title="Turmas" subtitle='jogue com a sua turma' />
 
-      <GroupCard title='Galera do Ignite' />
-      <GroupCard title='Galera do barulho' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
     </GroupsContainer>
   )
 }
