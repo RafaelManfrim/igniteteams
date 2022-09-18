75aa84fde5a1a025952a5148ee1b6dadb444c823
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-09-18 08:38:16 -0300

    Tratando erros e exibindo para o usuário

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,11 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
+import { Alert } from 'react-native';
 
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { createGroup } from '@storage/group/createGroup';
+import { AppError } from '@utils/AppError';
 
 import { NewGroupContainer, NewGroupContent, NewGroupIcon } from './styles';
 
@@ -15,8 +17,21 @@ export function NewGroup() {
   const navigation = useNavigation()
 
   async function handleCreateGroup() {
-    await createGroup(groupName)
-    navigation.navigate('players', { group: groupName })
+    try {
+      if (groupName.trim().length === 0) {
+        throw new AppError('Informe o nome do grupo');
+      }
+
+      await createGroup(groupName)
+      navigation.navigate('players', { group: groupName })
+    } catch (err) {
+      if (err instanceof AppError) {
+        Alert.alert('Novo Grupo', err.message)
+      } else {
+        Alert.alert('Erro Inesperado', 'Não foi possível criar o grupo')
+        console.log(err)
+      }
+    }
   }
 
   return (
--- src/storage/group/createGroup.ts
+++ src/storage/group/createGroup.ts
@@ -1,12 +1,20 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { GROUP_COLLECTION } from '@storage/config'
+import { AppError } from '@utils/AppError'
 
 import { getAllGroups } from './getAllGroups'
 
 export async function createGroup(newGroupName: string) {
   try {
     const storedGroups = await getAllGroups()
+
+    const groupAlreadyExists = storedGroups.includes(newGroupName)
+
+    if (groupAlreadyExists) {
+      throw new AppError('O grupo digitado já existe')
+    }
+
     const storage = JSON.stringify([...storedGroups, newGroupName])
     await AsyncStorage.setItem(GROUP_COLLECTION, storage)
   } catch (err) {
