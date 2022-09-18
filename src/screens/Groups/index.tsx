7fd8232a6e04da6e903de41629e63fc087de4fef
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-09-18 08:41:10 -0300

    Abrindo grupo direto da screen Groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -29,6 +29,10 @@ export function Groups() {
     }
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
   useFocusEffect(useCallback(() => {
     fetchGroups()
   }, []))
@@ -42,7 +46,7 @@ export function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => (
-          <GroupCard title={item} />
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
         )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira turma?" />}
